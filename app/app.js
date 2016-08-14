5c505e6490a3dcf680ace8947fa25bee98cd3267
Maigo Erit <maigo.erit@smit.ee>
2016-08-14 23:41:00 +0300

    update dependencies. fix restarting on change

--- app/app.js
+++ app/app.js
@@ -2,19 +2,20 @@ if (require('electron-squirrel-startup')) {
     return;
 }
 
-var app = require('electron').app
-var notifier = require('node-notifier')
+var app = require('electron').app;
+var notifier = require('node-notifier');
 
 
-var pluginMgr = require('./plugin-manager')
-var backgroundService = require('./background.service')
+var pluginMgr = require('./plugin-manager');
+var backgroundService = require('./background.service');
 var ipcMain = require('electron').ipcMain;
 
-var config = require('./config')
+var config = require('./config');
+
 require("electron").crashReporter.start(config.crashOpts)
 
 var AutoLaunch = require('auto-launch');
-var mb = require('./tray')
+var mb = require('./tray');
 
 var appLauncher = new AutoLaunch({
     name: 'Backer Timetracker'
