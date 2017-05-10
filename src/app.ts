0a17e6e89a8340109b9c10b4bd150af25493fc82
Maigo Erit <maigo.erit@smit.ee>
2017-05-10 08:19:10 +0300

    hide menubar in mainwindow menu

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,21 @@
-import {Router, RouterConfiguration} from "aurelia-router";
+import { Router, RouterConfiguration } from "aurelia-router";
+
+let mainConfig: any = (<any>window).nodeRequire('./app/config');
 
 export class App {
-    router:Router;
+    router: Router;
 
-    configureRouter(config:RouterConfiguration, router:Router) {
+    configureRouter(config: RouterConfiguration, router: Router) {
         config.title = 'Tockler';
         config.map([
-            {route: ['menubar'], name: 'menubar', moduleId: './menubar/menubar', nav: true, title: 'Menubar'},
-            {route: ['','timeline'], name: 'timeline', moduleId: './timeline/timeline-view', nav: true, title: 'Timeline'},
-            {route: 'summary', name: 'summary', moduleId: './summary/summary', nav: true, title: 'Summary'},
-            {route: 'settings', name: 'settings', moduleId: './settings/settings', nav: true, title: 'Settings'}
+            {
+                route: ['menubar'], name: 'menubar', moduleId: './menubar/menubar',
+                nav: (mainConfig.isDev && !mainConfig.trayEnabledInDev),
+                title: 'Menubar'
+            },
+            { route: ['', 'timeline'], name: 'timeline', moduleId: './timeline/timeline-view', nav: true, title: 'Timeline' },
+            { route: 'summary', name: 'summary', moduleId: './summary/summary', nav: true, title: 'Summary' },
+            { route: 'settings', name: 'settings', moduleId: './settings/settings', nav: true, title: 'Settings' }
         ]);
 
         this.router = router;
