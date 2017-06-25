128d5a5ba6e763b4d1e8c0e8bd84e15988ddd0e3
Maigo Erit <maigo.erit@smit.ee>
2017-06-25 09:54:22 +0300

    replace bunyan with electron-log

--- app/log-manager.ts
+++ app/log-manager.ts
@@ -1,43 +1,20 @@
 import { app } from "electron";
 import * as path from 'path';
-import * as bunyan from 'bunyan';
+import * as electronLog from 'electron-log';
+
+electronLog.transports.console.level = 'info';
 
 export class LogManager {
     logger;
 
     constructor() {
-        this.init({ userDir: app.getPath('userData') });
     }
 
     init(settings) {
-        if (!this.logger) {
-
-            var outputPath = path.join(settings.userDir, 'stdout.json');
-            // Create logger
-            this.logger = bunyan.createLogger({
-                name: 'tockler',
-                streams: [
-                    {
-                        level: 'info',
-                        stream: process.stdout            // log INFO and above to stdout
-                    },
-                    {
-                        level: 'debug',
-                        type: 'rotating-file',
-                        period: '1h',
-                        count: 50,        // keep 3 back copies
-                        path: outputPath
-                    }
-                ]
-            });
-
-            this.logger.info('Saving logs in directory:' + settings.userDir);
-
-        }
+       
     }
     getLogger(name) {
-        //console.log("Getting logger for: " + name);
-        return this.logger.child({ logger_name: name });
+        return electronLog;
     };
 }
 
