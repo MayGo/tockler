485638bb1e8a49929320cbbcad373f4750a1b9b0
Maigo Erit <maigo.erit@smit.ee>
2017-07-03 09:34:06 +0300

    white border on big icon.

--- app/config.ts
+++ app/config.ts
@@ -15,7 +15,7 @@ export default {
     userDir: userDir,
 
     icon: path.join(root, 'shared/img/icon/tockler_icon.png'),
-    iconBig: path.join(root, 'shared/img/icon/tockler_icon_big.png'),
+    iconBig: path.join(root, 'shared/img/icon/tockler_icon_big_w_bg.png'),
 
     // plugins directory
     pluginsPath: root,
