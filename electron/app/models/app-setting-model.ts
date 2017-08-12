df893d47e71bdaebf6b4cd91026c002dabff99a9
Maigo Erit <maigo.erit@smit.ee>
2017-08-13 00:27:13 +0300

    Add index to app-setting name

--- electron/app/models/app-setting-model.ts
+++ electron/app/models/app-setting-model.ts
@@ -1,16 +1,30 @@
 /* tslint:disable:variable-name */
 
-import * as SequelizeStatic from "sequelize";
-import { DataTypes, Sequelize } from "sequelize";
-import { AppSettingAttributes, AppSettingInstance } from "./interfaces/app-setting-interface";
+import * as SequelizeStatic from 'sequelize';
+import { DataTypes, Sequelize } from 'sequelize';
+import {
+  AppSettingAttributes,
+  AppSettingInstance,
+} from './interfaces/app-setting-interface';
 
-export default function (sequelize: Sequelize):
-  SequelizeStatic.Model<AppSettingInstance, AppSettingAttributes> {
-  let AppSetting = sequelize.define<AppSettingInstance, AppSettingAttributes>('AppSetting', {
-    name: SequelizeStatic.STRING,
-    color: SequelizeStatic.STRING
-  }, { timestamps: false });
+export default function(
+  sequelize: Sequelize,
+): SequelizeStatic.Model<AppSettingInstance, AppSettingAttributes> {
+  let AppSetting = sequelize.define<AppSettingInstance, AppSettingAttributes>(
+    'AppSetting',
+    {
+      name: SequelizeStatic.STRING,
+      color: SequelizeStatic.STRING,
+    },
+    {
+      timestamps: false,
+      indexes: [
+        {
+          fields: ['name'],
+        },
+      ],
+    },
+  );
 
   return AppSetting;
 }
-
