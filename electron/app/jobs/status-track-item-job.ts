65b71154dd083c1e8cc9c347584d8e7cd23b49dc
Maigo Erit <maigo.erit@smit.ee>
2017-09-08 22:22:33 +0300

    c3.1.3 add more types. handleError(err.toString());

--- electron/app/jobs/status-track-item-job.ts
+++ electron/app/jobs/status-track-item-job.ts
@@ -1,116 +1,109 @@
-
 import { logManager } from '../log-manager';
 import { stateManager } from '../state-manager';
 let logger = logManager.getLogger('LogTrackItemJob');
 
 import * as moment from 'moment';
-import { TrackItemType } from "../enums/track-item-type";
+import { TrackItemType } from '../enums/track-item-type';
 import { backgroundService } from '../background-service';
-import BackgroundUtils from "../background-utils";
-import config from "../config";
+import BackgroundUtils from '../background-utils';
+import config from '../config';
 import * as path from 'path';
-import UserMessages from "../user-messages";
+import UserMessages from '../user-messages';
 
-import { exec, execSync, execFile } from "child_process";
-import { State } from "../enums/state";
-import { appConstants } from "../app-constants";
+import { exec, execSync, execFile } from 'child_process';
+import { State } from '../enums/state';
+import { appConstants } from '../app-constants';
 
 export class StatusTrackItemJob {
+  run() {
+    try {
+      this.checkIfIsInCorrectState();
+      this.saveUserIdleTime();
+    } catch (error) {
+      logger.error(error);
+    }
+  }
 
-    run() {
-        try {
-            this.checkIfIsInCorrectState();
-            this.saveUserIdleTime();
-        } catch (error) {
-            logger.error(error);
-        }
+  checkIfIsInCorrectState(): void {}
+
+  saveUserIdleTime() {
+    //'darwin', 'freebsd', 'linux', 'sunos' or 'win32'
+    let runExec = '';
+    let args = [];
+    if (process.platform === 'darwin') {
+      runExec = 'sh';
+      args.push(path.join(config.root, 'scripts', 'get-user-idle-time.mac.sh'));
+    } else if (process.platform === 'win32') {
+      runExec = 'powershell.exe';
+      args.push(
+        '"& ""' +
+          path.join(config.root, 'scripts', 'get-user-idle-time.ps1') +
+          '"""',
+      );
+    } else if (process.platform === 'linux') {
+      runExec = 'sh';
+      args.push(
+        path.join(config.root, 'scripts', 'get-user-idle-time.linux.sh'),
+      );
     }
 
-    checkIfIsInCorrectState(): void {
+    //logger.debug('Script saveUserIdleTime file: ' + script)
 
+    let handleSuccess = stdout => {
+      logger.debug('Idle time: ' + stdout);
+
+      let seconds = stdout;
+
+      this.saveIdleTrackItem(seconds);
+    };
+
+    let handleError = (error: string) => {
+      logger.error('saveUserIdleTime error: ', error);
+      UserMessages.showError('Error getting user idle time', error);
+    };
+
+    let callcack = (err: Error, stdout: any, stderr: string) => {
+      if (stderr) {
+        handleError(stderr);
+        return;
+      }
+
+      if (err) {
+        handleError(err.toString());
+        logger.error('saveUserIdleTime err', err);
+        return;
+      }
+
+      handleSuccess(stdout);
+    };
+
+    execFile(runExec, args, { timeout: 2000 }, callcack);
+  }
+
+  saveIdleTrackItem(seconds) {
+    if (stateManager.isSystemSleeping()) {
+      logger.info('Computer is sleeping, not running saveIdleTrackItem');
+      return 'SLEEPING';
     }
 
-    saveUserIdleTime() {
-
-        //'darwin', 'freebsd', 'linux', 'sunos' or 'win32'
-        let runExec = "";
-        let args = [];
-        if (process.platform === 'darwin') {
-            runExec = "sh";
-            args.push(path.join(config.root, "scripts", "get-user-idle-time.mac.sh"));
-        } else if (process.platform === 'win32') {
-            runExec = 'powershell.exe';
-            args.push('"& ""' + path.join(config.root, "scripts", "get-user-idle-time.ps1") + '"""');
-        } else if (process.platform === 'linux') {
-            runExec = "sh";
-            args.push(path.join(config.root, "scripts", "get-user-idle-time.linux.sh"));
-        }
-
-        //logger.debug('Script saveUserIdleTime file: ' + script)
-
-        let handleSuccess = (stdout) => {
-            logger.debug('Idle time: ' + stdout);
-
-            let seconds = stdout;
-
-            this.saveIdleTrackItem(seconds);
-        };
-
-        let handleError = (error) => {
-            logger.error('saveUserIdleTime error: ' + error);
-
-            /* if (error.includes('UnauthorizedAccess') || error.includes('AuthorizationManager check failed')) {
-                 error = 'Choose [A] Always run in opened command prompt.';
-                 execSync('start cmd.exe  /K' + script);
-             }*/
-
-            UserMessages.showError('Error getting user idle time', error);
-
-        };
-
-        let callcack = (err, stdout, stderr) => {
-
-            if (stderr) {
-                handleError(stderr);
-                return;
-            }
-
-            if (err) {
-                handleError(err);
-                logger.error("saveUserIdleTime err", err);
-                return;
-            }
-
-            handleSuccess(stdout);
-        };
-
-        execFile(runExec, args, { timeout: 2000 }, callcack);
+    let state: State =
+      seconds > appConstants.IDLE_IN_SECONDS_TO_LOG ? State.Idle : State.Online;
+    // Cannot go from OFFLINE to IDLE
+    if (stateManager.isSystemOffline() && state === State.Idle) {
+      logger.info('Not saving. Cannot go from OFFLINE to IDLE');
+      return 'BAD_STATE';
     }
 
-    saveIdleTrackItem(seconds) {
+    let rawItem: any = {
+      taskName: 'StatusTrackItem',
+      app: state,
+      title: state.toString().toLowerCase(),
+      beginDate: BackgroundUtils.currentTimeMinusJobInterval(),
+      endDate: new Date(),
+    };
 
-        if (stateManager.isSystemSleeping()) {
-            logger.info('Computer is sleeping, not running saveIdleTrackItem');
-            return 'SLEEPING';
-        }
-
-        let state: State = (seconds > appConstants.IDLE_IN_SECONDS_TO_LOG) ? State.Idle : State.Online;
-        // Cannot go from OFFLINE to IDLE
-        if (stateManager.isSystemOffline() && state === State.Idle) {
-            logger.info('Not saving. Cannot go from OFFLINE to IDLE');
-            return 'BAD_STATE';
-        }
-
-        let rawItem: any = {
-            taskName: 'StatusTrackItem',
-            app: state,
-            title: state.toString().toLowerCase(),
-            beginDate: BackgroundUtils.currentTimeMinusJobInterval(),
-            endDate: new Date()
-        };
-
-        backgroundService.createOrUpdate(rawItem);
-    }
+    backgroundService.createOrUpdate(rawItem);
+  }
 }
 
 export const statusTrackItemJob = new StatusTrackItemJob();
--- electron/app/user-messages.ts
+++ electron/app/user-messages.ts
@@ -1,44 +1,45 @@
 'use strict';
 
-import * as notifier from "node-notifier";
-import * as path from "path";
-import * as moment from "moment";
+import * as notifier from 'node-notifier';
+import * as path from 'path';
+import * as moment from 'moment';
 
-import config from "./config";
+import config from './config';
 
-import {logManager} from "./log-manager";
+import { logManager } from './log-manager';
 
 const logger = logManager.getLogger('UserMessages');
 
 const iconUrl = config.iconBig;
 
 export default class UserMessages {
-    static lastError = '';
-    static lastTime = moment();
+  static lastError = '';
+  static lastTime = moment();
 
-    constructor() {
+  constructor() {}
+  static showError(title: string, error: string) {
+    let diffInMinutes = moment().diff(UserMessages.lastTime, 'minutes');
 
+    if (UserMessages.lastError === error && diffInMinutes < 1) {
+      logger.debug('Same error message, waiting one minute');
+      return;
     }
-    static showError(title, error) {
-        let diffInMinutes = moment().diff(UserMessages.lastTime, 'minutes');
 
-        if (UserMessages.lastError === error && diffInMinutes < 1) {
-            logger.debug('Same error message, waiting one minute');
-            return;
-        }
+    UserMessages.lastError = error;
+    UserMessages.lastTime = moment();
 
-        UserMessages.lastError = error;
-        UserMessages.lastTime = moment();
-
-        notifier.notify({
-            title: title,
-            message: error,
-            icon: iconUrl,
-            sound: true, // Only Notification Center or Windows Toasters
-            wait: false // Wait with callback, until user action is taken against notification
-        }, function (err, response) {
-            logger.error('Notifier error:', err, response);
-            //UserMessages.lastError = '';
-        });
-    }
+    notifier.notify(
+      {
+        title: title,
+        message: error,
+        icon: iconUrl,
+        sound: true, // Only Notification Center or Windows Toasters
+        wait: false, // Wait with callback, until user action is taken against notification
+      },
+      function(err, response) {
+        logger.error('Notifier error:', err, response);
+        //UserMessages.lastError = '';
+      },
+    );
+  }
 }
