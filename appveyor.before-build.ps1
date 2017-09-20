f0b07e80c0aee4d2aae1ee18287168a4d0c2086c
Maigo Erit <maigo.erit@smit.ee>
2017-09-20 23:33:39 +0300

    signtool env

--- appveyor.before-build.ps1
+++ appveyor.before-build.ps1
@@ -1,7 +1,7 @@
 # Get build path
 $path = $env:APPVEYOR_BUILD_FOLDER;
 
-$sign = "signtool sign /v /ac .\appveyor-tockler.cer";
+$sign = "`"${env:SIGNTOOL}`"  sign /v /ac .\appveyor-tockler.cer";
 
 cmd.exe /c "$sign $path\electron\script\get-foreground-window-title.ps1" ;
 cmd.exe /c "$sign $path\electron\script\get-user-idle-time.ps1" ;
