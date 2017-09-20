30654a6b90b7b1904b87df322db148c51d4a6843
Maigo Erit <maigo.erit@smit.ee>
2017-09-20 23:22:07 +0300

    use signtool

--- appveyor.before-build.ps1
+++ appveyor.before-build.ps1
@@ -1,7 +1,7 @@
 # Get build path
 $path = $env:APPVEYOR_BUILD_FOLDER;
 
-$sign = "`"${env:SIGNTOOL}`"  sign /v /ac .\appveyor-tockler.cer";
+$sign = "signtool sign /v /ac .\appveyor-tockler.cer";
 
 cmd.exe /c "$sign $path\electron\script\get-foreground-window-title.ps1" ;
 cmd.exe /c "$sign $path\electron\script\get-user-idle-time.ps1" ;
