3f786b255f6afee897b2f98693a941495320a1e3
Maigo Erit <maigo.erit@gmail.com>
2017-09-21 10:54:20 +0300

    Update appveyor.before-build.ps1

--- appveyor.before-build.ps1
+++ appveyor.before-build.ps1
@@ -1,7 +1,7 @@
 # Get build path
 $path = $env:APPVEYOR_BUILD_FOLDER;
 
-$certSelfSigned = New-SelfSignedCertificate -DnsName trimatech.ee -CertStoreLocation Cert:\CurrentUser\My -Type CodeSigningCert -KeySpec Signature
+$certSelfSigned = New-SelfSignedCertificate -DnsName trimatech.ee -CertStoreLocation Cert:\CurrentUser\My -KeyExportPolicy Exportable -Type CodeSigningCert -KeySpec Signature
 Set-AuthenticodeSignature -FilePath "$path\electron\scripts\get-foreground-window-title.ps1" -Certificate $certSelfSigned 
 Set-AuthenticodeSignature -FilePath "$path\electron\scripts\get-user-idle-time.ps1" -Certificate $certSelfSigned 
 
