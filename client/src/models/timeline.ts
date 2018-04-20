807d4ad0373c9423063a60bfaee90fb81c1319d3
MayGo <maigo.erit@gmail.com>
2018-04-20 22:22:07 +0300

    refactor

--- client/src/models/timeline.ts
+++ client/src/models/timeline.ts
@@ -1,44 +1,7 @@
 import * as moment from 'moment';
 import { TrackItemService } from '../services/TrackItemService';
 import { AppSettingService } from '../services/AppSettingService';
-import { TrackItemType } from '../enum/TrackItemType';
-import { ITrackItem } from '../@types/ITrackItem';
-import { ITimelineState } from '../@types/ITimelineState';
-
-const handleTimelineItems = (
-    state: ITimelineState,
-    payload: {
-        appTrackItems: object;
-        logTrackItems: object;
-        statusTrackItems: object;
-    },
-): ITimelineState => {
-    return {
-        ...state,
-        [TrackItemType.AppTrackItem]: payload.appTrackItems,
-        [TrackItemType.LogTrackItem]: payload.logTrackItems,
-        [TrackItemType.StatusTrackItem]: payload.statusTrackItems,
-    };
-};
-
-const addToTimelineItems = (
-    state: ITimelineState,
-    payload: {
-        appItems: ITrackItem[];
-        logItems: ITrackItem[];
-        statusItems: ITrackItem[];
-    },
-): ITimelineState => {
-    return {
-        ...state,
-        [TrackItemType.AppTrackItem]: [...state[TrackItemType.AppTrackItem], ...payload.appItems],
-        [TrackItemType.LogTrackItem]: [...state[TrackItemType.LogTrackItem], ...payload.logItems],
-        [TrackItemType.StatusTrackItem]: [
-            ...state[TrackItemType.StatusTrackItem],
-            ...payload.statusItems,
-        ],
-    };
-};
+import { handleTimelineItems, addToTimelineItems } from './timeline.util';
 
 export const timelineModel: any = {
     namespace: 'timeline',
