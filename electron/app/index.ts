7a016a3fb2b5bfe7887e8c04a3e4eb23aec81314
MayGo <maigo.erit@gmail.com>
2018-06-11 22:39:06 +0300

    add window-all-closed back. without it it closed all app. Add esc shortcut to get out of fullscreen

--- electron/app/index.ts
+++ electron/app/index.ts
@@ -1,6 +1,6 @@
 import { backgroundJob } from './background-job';
 import { backgroundService } from './background-service';
-import { app, ipcMain, powerMonitor } from 'electron';
+import { app, ipcMain, powerMonitor, globalShortcut } from 'electron';
 import { logManager } from './log-manager';
 
 import AppManager from './app-manager';
@@ -39,6 +39,12 @@ app.on('ready', async () => {
         await extensionsManager.init();
     }
 
+    const ret = globalShortcut.register('Escape', function() {
+        console.log('Escape is pressed');
+
+        windowManager.mainWindow.setFullScreen(false);
+    });
+
     windowManager.setMainWindow();
     windowManager.initMainWindowEvents();
 
@@ -68,6 +74,17 @@ require('electron-context-menu')({});
 ipcMain.on('close-app', function() {
     console.log('Closing app');
     app.quit();
+});
+
+app.on('window-all-closed', function() {
+    console.log('window-all-closed');
+    // pluginMgr.removeAll();
+    // app.quit();
+});
+
+app.on('will-quit', () => {
+    // Unregister all shortcuts.
+    globalShortcut.unregisterAll();
 });
 
 /* Single Instance Check */
