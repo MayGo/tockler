c14dd413cb53fe195fa6715a0ecdd64f35d5257e
MayGo <maigo.erit@gmail.com>
2018-06-24 22:43:41 +0300

    3.3.19 update rest of plugins (electron)

--- electron/webpack.config.js
+++ electron/webpack.config.js
@@ -1,46 +1,38 @@
 const path = require('path');
 const os = require('os');
 
-const { optimize: { CommonsChunkPlugin }, ProvidePlugin } = require('webpack');
-const {
-  TsConfigPathsPlugin,
-  CheckerPlugin,
-} = require('awesome-typescript-loader');
+const { TsConfigPathsPlugin, CheckerPlugin } = require('awesome-typescript-loader');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  target: 'electron-main',
+    target: 'electron-main',
 
-  externals: [nodeExternals()],
+    externals: [nodeExternals()],
 
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  entry: {
-    index: path.resolve(__dirname, 'app', 'index.ts'),
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-  },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    entry: {
+        index: path.resolve(__dirname, 'app', 'index.ts'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+    },
 
-  node: {
-    __dirname: false,
-    __filename: false,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/i,
-        loader: 'awesome-typescript-loader',
-      },
-    ],
-  },
-  plugins: [
-    new FriendlyErrorsWebpackPlugin(),
-    new TsConfigPathsPlugin(),
-    new CheckerPlugin(),
-  ],
+    node: {
+        __dirname: false,
+        __filename: false,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/i,
+                loader: 'awesome-typescript-loader',
+            },
+        ],
+    },
+    plugins: [new FriendlyErrorsWebpackPlugin(), new TsConfigPathsPlugin(), new CheckerPlugin()],
 };
