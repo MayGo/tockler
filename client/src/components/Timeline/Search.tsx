0340faca4b17addcc110e2ee3bf83cdaf6e7b186
MayGo <maigo.erit@gmail.com>
2019-04-13 22:30:27 +0300

    Add buttons two go forward  one day and back one day

--- client/src/components/Timeline/Search.tsx
+++ client/src/components/Timeline/Search.tsx
@@ -13,6 +13,9 @@ interface IHocProps {}
 
 type IFullProps = IProps & IHocProps;
 
+const getDayBefore = d => moment(d).subtract(1, 'days');
+const getDayAfter = d => moment(d).add(1, 'days');
+
 export class Search extends React.PureComponent<IFullProps, IProps> {
     constructor(props: any) {
         super(props);
@@ -30,25 +33,37 @@ export class Search extends React.PureComponent<IFullProps, IProps> {
             console.error('No dates selected');
         }
     };
+
     selectToday = () => {
         console.log('Select today');
         const beginDate = moment().startOf('day');
         const endDate = moment().endOf('day');
-        const newTimerange = [beginDate, endDate];
-        this.props.loadTimerange(newTimerange);
+
+        this.props.loadTimerange([beginDate, endDate]);
     };
+
     selectYesterday = () => {
         console.log('Select today');
-        const beginDate = moment()
-            .startOf('day')
-            .subtract(1, 'days');
+        const beginDate = getDayBefore(moment().startOf('day'));
+        const endDate = getDayBefore(moment().endOf('day'));
+        this.props.loadTimerange([beginDate, endDate]);
+    };
 
-        const endDate = moment()
-            .endOf('day')
-            .subtract(1, 'days');
+    goBackOneDay = () => {
+        const { timerange } = this.props;
+        console.log('goBackOneDay');
+        const beginDate = getDayBefore(moment(timerange[0]));
+        const endDate = getDayBefore(moment(timerange[1]));
 
-        const newTimerange = [beginDate, endDate];
-        this.props.loadTimerange(newTimerange);
+        this.props.loadTimerange([beginDate, endDate]);
+    };
+    goForwardOneDay = () => {
+        const { timerange } = this.props;
+        console.log('goForwardOneDay');
+        const beginDate = getDayAfter(moment(timerange[0]));
+        const endDate = getDayAfter(moment(timerange[1]));
+
+        this.props.loadTimerange([beginDate, endDate]);
     };
 
     selectHour = () => {
@@ -71,8 +86,15 @@ export class Search extends React.PureComponent<IFullProps, IProps> {
                     <Button onClick={this.selectYesterday}>Yesterday</Button>
                 </Box>
                 <Box px={1}>
+                    <Button onClick={this.goBackOneDay}>{'<'}</Button>
+                </Box>
+                <Box px={1}>
                     <RangePicker value={timerange} onChange={this.onChange} />
                 </Box>
+                <Box px={1}>
+                    <Button onClick={this.goForwardOneDay}>{'>'}</Button>
+                </Box>
+
                 <Box px={1}>
                     <Button onClick={this.selectToday}>Today</Button>
                 </Box>
