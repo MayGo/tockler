9bdd2a4df4f5976890a93546c810971fe08a2bd5
d <maigo.erit@gmail.com>
2020-02-16 12:37:43 +0200

    fix preload app version

--- electron/preloadStuff.js
+++ electron/preloadStuff.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const Store = require('electron-store');
-const { app, ipcRenderer } = require('electron');
+const electron = require('electron');
 const Sentry = require('@sentry/electron');
 
-const version = app.getVersion();
+const version = electron.remote.app.getVersion();
 
 Sentry.init({
     dsn: 'https://8b5e35e414d146afac47bbf66d904746@sentry.io/2004797',
@@ -15,4 +15,4 @@ Sentry.init({
 window.Sentry = Sentry;
 window.version = version;
 window.Store = Store;
-window.ipcRenderer = ipcRenderer;
+window.ipcRenderer = electron.ipcRenderer;
