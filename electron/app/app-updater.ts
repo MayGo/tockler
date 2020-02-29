d6d1ca4dcc4daa940f3eb9a8624e545389d925b5
Maigo Erit <merit@intertrust.com>
2020-02-29 11:57:11 +0200

    Don't show network errors on auto update as notification

--- electron/app/app-updater.ts
+++ electron/app/app-updater.ts
@@ -11,6 +11,17 @@ const logger = logManager.getLogger('AppUpdater');
 const ONE_MINUTE_MS = 60 * 1000;
 export const CHECK_INTERVAL_MS = ONE_MINUTE_MS * 30;
 
+function isNetworkError(errorObject) {
+    return (
+        errorObject.message === 'net::ERR_INTERNET_DISCONNECTED' ||
+        errorObject.message === 'net::ERR_PROXY_CONNECTION_FAILED' ||
+        errorObject.message === 'net::ERR_CONNECTION_RESET' ||
+        errorObject.message === 'net::ERR_CONNECTION_CLOSE' ||
+        errorObject.message === 'net::ERR_NAME_NOT_RESOLVED' ||
+        errorObject.message === 'net::ERR_CONNECTION_TIMED_OUT'
+    );
+}
+
 export default class AppUpdater {
     static init() {
         if (config.isDev) {
@@ -31,8 +42,16 @@ export default class AppUpdater {
             });
         });
 
-        autoUpdater.on('error', err => {
-            showNotification({ title: 'Tockler error', body: err ? err.stack || err : 'unknown' });
+        autoUpdater.on('error', e => {
+            if (isNetworkError(e)) {
+                logger.debug('Network error:', e);
+            } else {
+                logger.error('AutoUpdater error:', e);
+                showNotification({
+                    title: 'Tockler error',
+                    body: e ? e.stack || e : 'unknown',
+                });
+            }
         });
 
         AppUpdater.checkIfEnabled();
@@ -65,8 +84,18 @@ export default class AppUpdater {
         showNotification({ body: `Checking for updates...`, silent: true });
 
         autoUpdater.on('update-not-available', AppUpdater.updateNotAvailable);
-        const result: UpdateCheckResult = await autoUpdater.checkForUpdatesAndNotify();
-        logger.debug(`Update result ${result.updateInfo.version}`);
+        try {
+            const result: UpdateCheckResult = await autoUpdater.checkForUpdatesAndNotify();
+
+            logger.debug(`Update result ${result?.updateInfo?.version}`);
+        } catch (e) {
+            logger.error('Error checking updates', e);
+            showNotification({
+                title: 'Tockler error',
+                body: e ? e.stack || e : 'unknown',
+            });
+        }
+
         autoUpdater.removeListener('update-not-available', AppUpdater.updateNotAvailable);
     }
 }
