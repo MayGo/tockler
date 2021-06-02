edf4ec50c0e1ead76ffa5bfdbb428b577d0c0241
Richard Guerre <guerre.richard@yahoo.com>
2021-06-02 14:10:49 +0200

    Add login alert + Change GITSTART_LOGIN_URL

--- client/src/components/MainLayout/MainLayout.tsx
+++ client/src/components/MainLayout/MainLayout.tsx
@@ -1,15 +1,38 @@
-import { Layout } from 'antd';
+import { Box } from 'reflexbox';
+import { Alert, Layout } from 'antd';
 import React from 'react';
 import { HeaderMenu } from './HeaderMenu';
+import { loginInExternalBrowser } from '../../services/settings.api';
 
 const { Footer, Content } = Layout;
 
 export function MainLayout({ children, location }: any) {
+    const isLoggedIn = false;
+
     return (
         <div>
             <Layout>
                 <HeaderMenu location={location} />
-                <Content>{children}</Content>
+                <Content>
+                    {!isLoggedIn &&
+                    (location.pathname === '/' ||
+                        location.pathname === '/app/timeline' ||
+                        location.pathname === '/app/timeline2' ||
+                        location.pathname === '/app/summary' ||
+                        location.pathname === '/app/search') ? (
+                        <Box p={1}>
+                            <Alert
+                                type="warning"
+                                message="You are currently not logged in to GitStart."
+                                description="Please login so that your events are synced to GitStart."
+                                showIcon
+                                closeText="Login"
+                                onClose={loginInExternalBrowser}
+                            />
+                        </Box>
+                    ) : null}
+                    {children}
+                </Content>
                 <Footer>
                     <a href="https://github.com/MayGo">MayGo</a> @ 2021
                 </Footer>
