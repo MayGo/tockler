6f1d77ea3b363d1f5bccbeef6f5256d8db9a9f4e
Richard Guerre <guerre.richard@yahoo.com>
2021-06-07 18:10:21 +0200

    add DB_JOB_INTERVAL in ENVs

--- electron/app/app-constants.ts
+++ electron/app/app-constants.ts
@@ -1,7 +1,9 @@
 export default class AppConstants {
     // TODO: Allow user to change this setting within the Settings view in MainAppPage so that they can reduce tockler's energy usage
     TIME_TRACKING_JOB_INTERVAL: number = 3 * 1000; // 3 seconds
-    DB_JOB_INTERVAL: number = 10 * 1000; // 5 minutes
+    DB_JOB_INTERVAL: number = process.env.DB_JOB_INTERVAL
+        ? parseInt(process.env.DB_JOB_INTERVAL)
+        : 5 * 60 * 1000; // 5 minutes
     IDLE_IN_SECONDS_TO_LOG: number = 60 * 1;
     PROTOCOL_NAME: string = 'x-gitstart-devtime';
 }
