6070870ea8a5b6b301d8031dafaee1cc09d5578d
ztangaj <ztangaj@connect.ust.hk>
2021-06-15 14:23:58 +0800

    add sentry.io to client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import 'typeface-berkshire-swash';
 import { MainRouter } from './router';
 import { setupFrontendListener } from 'eiphop';
 import { AppDataProvider } from './routes/AppDataProvider';
 import { StoreProvider } from 'easy-peasy';
 import { mainStore } from './store/mainStore';
+require('dotenv').config();
 
 (window as any).CSPSettings = {
     nonce: 'nonce',
 };
 const { ipcRenderer } = window as any;
+
+Sentry.init({ 
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
+    environment: process.env.NODE_ENV,
+});
+console.log('init');
+console.log(process.env);
 
 setupFrontendListener({ ipcRenderer } as any);
 
@@ -21,12 +33,12 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 ReactDOM.render(
-    <StoreProvider store={mainStore}>
-        <AppDataProvider>
-            <Router>
-                <MainRouter />
-            </Router>
-        </AppDataProvider>
-    </StoreProvider>,
+        <StoreProvider store={mainStore}>
+            <AppDataProvider>
+                <Router>
+                    <MainRouter />
+                </Router>
+            </AppDataProvider>
+        </StoreProvider>,
     document.getElementById('root') as HTMLElement,
 );
