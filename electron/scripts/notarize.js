ac33f7999833efceecfdb3d02a21fe8b781c9a53
Maigo Erit <maigo.erit@gmail.com>
2021-07-05 21:52:12 +0300

    update notarize settings

--- electron/scripts/notarize.js
+++ electron/scripts/notarize.js
@@ -2,16 +2,38 @@ require('dotenv').config();
 const { notarize } = require('electron-notarize');
 
 exports.default = async function notarizing(context) {
-    const { electronPlatformName, appOutDir } = context;
+    const { electronPlatformName, appOutDir, outDir, packager } = context;
+
+    // Don't notarize on pull requests
+    if (
+        process.env.GITHUB_EVENT_NAME == 'pull_request' ||
+        process.env.TRAVIS_EVENT_TYPE == 'pull_request' ||
+        process.env.APPVEYOR_PULL_REQUEST_NUMBER > 0
+    ) {
+        return;
+    }
+
     if (electronPlatformName !== 'darwin') {
         return;
     }
 
-    const appName = context.packager.appInfo.productFilename;
+    if (
+        packager.config.extraMetadata &&
+        packager.config.extraMetadata.irccloud &&
+        packager.config.extraMetadata.irccloud.local_build
+    ) {
+        return;
+    }
+
+    const appName = packager.appInfo.productFilename;
+    const appPath = `${appOutDir}/${appName}.app`;
+    const appBundleId = packager.config.appId;
+
+    console.info('notarizing', { appBundleId: appBundleId, appPath: appPath });
 
     return await notarize({
-        appBundleId: 'ee.trimatech.tockler',
-        appPath: `${appOutDir}/${appName}.app`,
+        appBundleId: appBundleId,
+        appPath: appPath,
         appleId: process.env.APPLEID,
         appleIdPassword: process.env.APPLEIDPASS,
     });
