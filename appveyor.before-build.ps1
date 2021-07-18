75da2cca8b525eec4b39475c56804e91ecc64ddf
Maigo Erit <maigo.erit@gmail.com>
2021-07-18 19:49:14 +0300

    remove some unused scripts

--- appveyor.before-build.ps1
+++
@@ -1,7 +0,0 @@
-# Get build path
-$path = $env:APPVEYOR_BUILD_FOLDER;
-
-$certSelfSigned = New-SelfSignedCertificate -DnsName trimatech.ee -CertStoreLocation Cert:\CurrentUser\My -KeyExportPolicy Exportable -Type CodeSigningCert -KeySpec Signature
-Set-AuthenticodeSignature -FilePath "$path\electron\scripts\get-foreground-window-title.ps1" -Certificate $certSelfSigned
-
-exit
--- electron/scripts/get-foreground-window-title.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-xprop -id $(xprop -root _NET_ACTIVE_WINDOW | cut -d ' ' -f 5) WM_NAME | cut -d '"' -f 2
