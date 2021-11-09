ba437517fb9dc9e3931f2618784522faf6ebb7d1
Maigo Erit <maigo.erit@gmail.com>
2021-11-09 11:30:29 +0200

    remove unused

--- client/src/components/TrayLayout/OnlineChart.tsx
+++ client/src/components/TrayLayout/OnlineChart.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext, useRef } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { VictoryContainer, VictoryPie } from 'victory';
 import { colorProp } from '../charts.utils';
 import { useChartThemeState } from '../../routes/ChartThemeProvider';
