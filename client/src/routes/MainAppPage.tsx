31278c8f1ecc2162c5e0ef70fefc1b5bfbe94bdb
MayGo <maigo.erit@gmail.com>
2022-01-16 11:49:50 +0200

    remove emailjs and use REST api directly

--- client/src/routes/MainAppPage.tsx
+++ client/src/routes/MainAppPage.tsx
@@ -6,20 +6,17 @@ import { TimelinePage } from './TimelinePage';
 import { TrayAppPage } from './TrayAppPage';
 import { SearchPage } from './SearchPage';
 import { SupportPage } from './SupportPage';
-import { UserProvider } from '../components/Paywall/UserProvider';
 
 export function MainAppPage() {
     return (
-        <UserProvider>
-            <Switch>
-                <Route exact path="/" component={TimelinePage} />
-                <Route path="/app/timeline" component={TimelinePage} />
-                <Route path="/app/settings" component={SettingsPage} />
-                <Route path="/app/summary" component={SummaryPage} />
-                <Route path="/app/support" component={SupportPage} />
-                <Route path="/app/search" component={SearchPage} />
-                <Route path="/app/trayApp" component={TrayAppPage} />
-            </Switch>
-        </UserProvider>
+        <Switch>
+            <Route exact path="/" component={TimelinePage} />
+            <Route path="/app/timeline" component={TimelinePage} />
+            <Route path="/app/settings" component={SettingsPage} />
+            <Route path="/app/summary" component={SummaryPage} />
+            <Route path="/app/support" component={SupportPage} />
+            <Route path="/app/search" component={SearchPage} />
+            <Route path="/app/trayApp" component={TrayAppPage} />
+        </Switch>
     );
 }
