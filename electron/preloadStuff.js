bf2eff4505386a9549b005afa2b2bfb3f4ab85e9
MayGo <maigo.erit@gmail.com>
2024-06-15 20:24:45 +0300

    update preloadStuff imports

--- electron/preloadStuff.js
+++ electron/preloadStuff.js
@@ -1,28 +1,28 @@
 'use strict';
-const { contextBridge, ipcRenderer } = require('electron');
-const Store = require('electron-store');
+import { contextBridge, ipcRenderer } from 'electron';
+import Store from 'electron-store';
 
-const log = require('electron-log');
-const Sentry = require('@sentry/electron');
+import log, { transports, debug } from 'electron-log';
+import Sentry, { init, showReportDialog, withScope, captureException, captureMessage } from '@sentry/electron';
 
 const config = new Store();
 
 if (process.env.NODE_ENV === 'production') {
-    Sentry.init({
+    init({
         dsn: process.env.SENTRY_DSN,
         environment: process.env.NODE_ENV,
         release: version,
         beforeSend(event) {
             // Check if it is an exception, if so, show the report dialog
             if (event.exception) {
-                Sentry.showReportDialog();
+                showReportDialog();
             }
             return event;
         },
     });
 }
 
-const origConsole = log.transports.console;
+const origConsole = transports.console;
 
 const isError = function (e) {
     return e && e.stack && e.message;
@@ -37,16 +37,16 @@ const sentryTransportConsole = (msgObj) => {
     if (!cachedErrors[message]) {
         cachedErrors[message] = true;
 
-        Sentry.withScope((scope) => {
+        withScope((scope) => {
             scope.setExtra('data', rest);
             scope.setExtra('date', msgObj.date.toLocaleTimeString());
             scope.setLevel(level);
             if (isError(message)) {
-                Sentry.captureException(message);
+                captureException(message);
             } else if (level === 'debug') {
                 // ignore debug for now
             } else {
-                Sentry.captureMessage(message);
+                captureMessage(message);
             }
         });
     }
@@ -54,18 +54,18 @@ const sentryTransportConsole = (msgObj) => {
     origConsole(msgObj);
 };
 
-log.transports.console = sentryTransportConsole;
+transports.console = sentryTransportConsole;
 
 const isProd = false;
-log.transports.console.level = isProd ? 'warn' : 'debug';
+transports.console.level = isProd ? 'warn' : 'debug';
 
 const IS_LOGGING_ENABLED = 'isLoggingEnabled';
 let isLoggingEnabled = config.get(IS_LOGGING_ENABLED);
 
 if (isLoggingEnabled) {
-    log.transports.file.level = 'debug';
+    transports.file.level = 'debug';
 } else {
-    log.transports.file.level = false;
+    transports.file.level = false;
 }
 
 const listeners = {};
@@ -85,18 +85,18 @@ contextBridge.exposeInMainWorld('electronBridge', {
         return await ipcRenderer.invoke(actionName, payload);
     },
     sendIpc: (key, ...args) => {
-        log.debug('Send message with key: ' + key, args);
+        debug('Send message with key: ' + key, args);
         ipcRenderer.send(key, ...args);
     },
     onIpc: (key, fn) => {
         const saferFn = (event, ...args) => fn(...args);
         // Deliberately strip event as it includes `sender`
-        log.debug('Add listener with key: ' + key);
+        debug('Add listener with key: ' + key);
         ipcRenderer.on(key, saferFn);
         listeners[key] = saferFn;
     },
     removeListenerIpc: (key) => {
-        log.debug('Remove listener with key: ' + key);
+        debug('Remove listener with key: ' + key);
         const fn = listeners[key];
         ipcRenderer.removeListener(key, fn);
         delete listeners[key];
