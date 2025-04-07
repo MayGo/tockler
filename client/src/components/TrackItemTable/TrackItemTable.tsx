3e7c50b9072cdb77015a1779abdbf171d5e44f5c
Maigo Erit <maigo.erit@gmail.com>
2025-04-07 20:12:36 +0300

    refactor: enhance duration calculations in TrackItemTable components
    - Updated the ItemsTable component to calculate subtotal using differenceInMilliseconds for improved accuracy.
    - Refactored the TrackItemTable component to directly filter items based on the visible timerange and calculate total duration accordingly.
    - Removed unused variables to streamline the code and enhance clarity.

--- client/src/components/TrackItemTable/TrackItemTable.tsx
+++ client/src/components/TrackItemTable/TrackItemTable.tsx
@@ -1,6 +1,7 @@
 import { Box } from '@chakra-ui/react';
 import { differenceInMilliseconds } from 'date-fns';
 import { sumBy } from 'lodash';
+import { ITrackItem } from '../../@types/ITrackItem';
 import { useStoreActions, useStoreState } from '../../store/easyPeasy';
 import { TIMERANGE_MODE_TODAY } from '../../store/mainStore';
 import { checkIfOneDay } from '../../timeline.util';
@@ -15,17 +16,14 @@ export const TrackItemTable = ({ type, resetButtonsRef }) => {
     const timerangeMode = useStoreState((state) => state.timerangeMode);
     const setLiveView = useStoreActions((actions) => actions.setLiveView);
 
-    // Get all data for the selected type filtered by visible timerange
-    const allFilteredData = filterItems(timeItems[type], visibleTimerange);
-
     // Use the first 10 items for the current page (since we're not doing server-side pagination here)
-    const data = allFilteredData;
+    const data = filterItems(timeItems[type], visibleTimerange);
 
     const isOneDay = checkIfOneDay(visibleTimerange);
     const isToday = timerangeMode === TIMERANGE_MODE_TODAY;
 
     // Calculate total duration in milliseconds for all the data
-    const totalDuration = sumBy(allFilteredData, (trackItem) => {
+    const totalDuration = sumBy(timeItems[type] as ITrackItem[], (trackItem) => {
         return differenceInMilliseconds(trackItem.endDate, trackItem.beginDate);
     });
 
