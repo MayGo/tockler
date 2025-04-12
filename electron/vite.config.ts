97728e0eeedaec34e99007a262cb399f40dd5fec
Maigo Erit <maigo.erit@gmail.com>
2025-04-12 21:28:40 +0300

    add electron as native

--- electron/vite.config.ts
+++ electron/vite.config.ts
@@ -48,7 +48,7 @@ export default defineConfig({
                 vite: {
                     build: {
                         rollupOptions: {
-                            external: [...builtinModules, 'better-sqlite3', 'active-win'],
+                            external: [...builtinModules, 'better-sqlite3', 'electron'],
                         },
                     },
                 },
